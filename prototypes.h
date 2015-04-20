17fc4ea12ea6d5cfa29d9b5da5fadf4bc75cc348
Nuttsrampage <valentindu71@gmail.com>
2015-04-21 00:29:34 +0200

    Structure du projet : crétation des différents header . Création de la fonction input (fini) + commentaires

---
+++ prototypes.h
@@ -0,0 +1,22 @@
+//Permet de lister tout les prototypes du projet et ainsi ne pas les oublier (il faut inclure ce fuchier a tout les autres )
+
+#include "structs.h"
+
+#ifndef PROTOTYPES
+#define PROTOTYPES
+
+#include "structs.h"
+
+/* Catalogue des prototypes des fonctions utilisées.
+
+
+extern void cleanup(void);
+extern void delay(unsigned int frameLimit);
+extern void drawGame(void);
+extern void gestionInputs(Input *input);
+extern void getInput(Input *input);
+extern SDL_Renderer *getrenderer(void);
+extern void init(char *);
+
+
+#endif
