e1abf259a330c096e4e4e3cddb9ed84af4f2d148
clemencemoulin <clemence.moulin@telecomnancy.net>
2015-05-14 12:49:55 +0200

    Présentation des menus V2

--- menu.c
+++ menu.c
@@ -31,26 +31,43 @@ void menu1(){
     //Si on appuie sur le bouton Quit
     if ((x > 300) && (x < 500) && (y > 290) && (y < 330) && leftclik = 1){
         //Fermer l'application
-
+        SDL_QUIT();
     }
 }
 
 // PAGE HELP
 void menuHelp(){
-    while (!((x > 300) && (x < 500) && (y > 150) && (y < 190) && leftclik == 1)){
-        menu2()
-    //Chargement de l'image
-    loadImage("graphics/Menu1.png");
-
-    //Si on appuie sur le bouton Play
-    if ((x > 300) && (x < 500) && (y > 150) && (y < 190) && leftclik == 1){
-        menu2();
-
+    while (!((x > 500) && (x < 700) && (y > 410) && (y < 440) && leftclik == 1)){
+        //Chargement de l'image
+        loadImage("graphics/MenuHelp.png");
     }
+    menu1();
 }
 // MENU 2
 void menu2(){
+    //Chargement de l'image
+    loadImage("graphics/Menu2.png");
 
+    //Si on appuie sur le bouton 1
+    if ((x > x1) && (x < x2) && (y > y1) && (y < 2) && leftclik == 1){
+        // Option de personnalisation 1
+
+    }
+    //Si on appuie sur le bouton 2
+    if ((x > x1) && (x < x2) && (y > y1) && (y < 2) && leftclik == 1){
+        // Option de personnalisation 1
+
+    }
+    //Si on appuie sur le bouton 3
+    if ((x > x1) && (x < x2) && (y > y1) && (y < 2) && leftclik == 1){
+        // Option de personnalisation 1
+
+    }
+    //Si on appuie sur le bouton Play
+    if ((x > 500) && (x < 700) && (y > 410) && (y < 440) && leftclik == 1){
+        // Lancer le jeu
+        menuGame();
+    }
 }
 
 // MENU JEU
@@ -61,11 +78,19 @@ void menuGame(){
 void menuWin(){
 
 }
-// PAGE LOST GAME
+// PAGE GAME OVER
 void menuLose(){
-
+    while (!(leftclik == 1)){
+        //Chargement de l'image
+        loadImage("graphics/MenuLose.png");
+    }
+    menu1();
 }
 // Gestion de la pause
 void menuBreak(){
-
+    while (!(leftclik == 1)){
+        //Chargement de l'image
+        loadImage("graphics/MenuLose.png");
+    }
+    menuGame();
 }
